dbd873c6749611991a0ee3ad8e34f19ce7fdd21e
Andrei Bas <bas.andrei@gmail.com>
2024-09-19 13:02:14 +0100

    Add camera switch button

--- src/components/video-feed.tsx
+++ src/components/video-feed.tsx
@@ -1,4 +1,6 @@
 import { useEffect, useRef, useState, MouseEvent } from "react";
+import { Button } from "./ui/button";
+import { SwitchCamera } from "lucide-react";
 
 function VideoFeed() {
   const videoRef = useRef<HTMLVideoElement | null>(null);
@@ -6,20 +8,28 @@ function VideoFeed() {
   const [pixelColor, setPixelColor] = useState<string>("rgb(0, 0, 0)");
   const [coordinates, setCoordinates] = useState<{ x: number; y: number }>({
     x: 160,
-    y: 120,
+    y: 160,
   });
+  const [facingMode, setFacingMode] = useState<"user" | "environment">("user");
 
-  useEffect(() => {
-    // Access the camera
+  const startCamera = () => {
     navigator.mediaDevices
-      .getUserMedia({ video: true })
+      .getUserMedia({ video: { facingMode: facingMode } })
       .then((stream) => {
         if (videoRef.current) {
           videoRef.current.srcObject = stream;
         }
       })
       .catch((err) => console.error("Error accessing the camera:", err));
-  }, []);
+  };
+
+  useEffect(() => {
+    startCamera();
+  }, [facingMode]);
+
+  const toggleCamera = () => {
+    setFacingMode((prevMode) => (prevMode === "user" ? "environment" : "user"));
+  };
 
   const processFrame = () => {
     if (canvasRef.current && videoRef.current) {
@@ -59,18 +69,23 @@ function VideoFeed() {
   };
 
   return (
-    <div className="flex flex-col items-center">
-      <video
-        ref={videoRef}
-        autoPlay
-        playsInline
-        className="w-80 h-60 bg-gray-700"
-        onClick={handleVideoClick}
-      ></video>
+    <div className="flex flex-col items-center mt-4">
+      <div className="relative w-80 h-80">
+        <video
+          ref={videoRef}
+          autoPlay
+          playsInline
+          className="absolute top-0 left-0 w-full h-full object-cover bg-gray-700"
+          onClick={handleVideoClick}
+        ></video>
+      </div>
+      <Button onClick={toggleCamera} variant="outline" className="mt-4">
+        <SwitchCamera />
+      </Button>
       <canvas
         ref={canvasRef}
         width="320"
-        height="240"
+        height="320"
         className="hidden"
       ></canvas>
       <div
