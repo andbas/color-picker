0bd5c42deb6e21b68b1e208704167a2f101f7412
Andrei Bas <bas.andrei@gmail.com>
2024-09-23 16:16:44 +0100

    A bit of formating

--- src/App.tsx
+++ src/App.tsx
@@ -44,49 +44,49 @@ function App() {
             />
           )}
         </div>
-        <Sheet open={menuOpen} onOpenChange={setMenuOpen}>
-          <SheetContent>
-            <SheetHeader>
-              <SheetTitle>Menu</SheetTitle>
-              <SheetDescription>
-                As soon as I'll have something to add here, I'll add it here.
-              </SheetDescription>
-            </SheetHeader>
-            <div className="text-sm text-muted-foreground my-10">
-              So great that you're using the app! Thanks for your support!
-            </div>
-            <SheetFooter>
-              <div className="flex gap-4 mx-auto">
-                <ModeToggle variant="ghost" />
-                <Button
-                  variant="ghost"
-                  size="icon"
-                  onClick={() => {
-                    setMenuOpen(false);
-                    setConsentOpen(true);
-                  }}
-                >
-                  <Cookie />
-                  <span className="sr-only">Consent Mode</span>
-                </Button>
-                <Button
-                  variant="ghost"
-                  size="icon"
-                  onClick={() =>
-                    window.open(
-                      "https://github.com/andbas/color-picker/",
-                      "_blank"
-                    )
-                  }
-                >
-                  <Github />
-                  <span className="sr-only">Github</span>
-                </Button>
-              </div>
-            </SheetFooter>
-          </SheetContent>
-        </Sheet>
       </div>
+      <Sheet open={menuOpen} onOpenChange={setMenuOpen}>
+        <SheetContent>
+          <SheetHeader>
+            <SheetTitle>Menu</SheetTitle>
+            <SheetDescription>
+              As soon as I'll have something to add here, I'll add it here.
+            </SheetDescription>
+          </SheetHeader>
+          <div className="text-sm text-muted-foreground my-10">
+            So great that you're using the app! Thanks for your support!
+          </div>
+          <SheetFooter>
+            <div className="flex gap-4 mx-auto">
+              <ModeToggle variant="ghost" />
+              <Button
+                variant="ghost"
+                size="icon"
+                onClick={() => {
+                  setMenuOpen(false);
+                  setConsentOpen(true);
+                }}
+              >
+                <Cookie />
+                <span className="sr-only">Consent Mode</span>
+              </Button>
+              <Button
+                variant="ghost"
+                size="icon"
+                onClick={() =>
+                  window.open(
+                    "https://github.com/andbas/color-picker/",
+                    "_blank"
+                  )
+                }
+              >
+                <Github />
+                <span className="sr-only">Github</span>
+              </Button>
+            </div>
+          </SheetFooter>
+        </SheetContent>
+      </Sheet>
       {consentOpen && (
         <Consent setConsent={setConsent} onOpenChange={setConsentOpen} />
       )}
