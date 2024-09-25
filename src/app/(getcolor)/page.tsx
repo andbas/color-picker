80bac0097c91e409fc31aca75a47797206f5c83b
Andrei Bas <bas.andrei@gmail.com>
2024-09-25 14:18:36 +0100

    Home page to launch the app

--- src/app/(getcolor)/page.tsx
+++ src/app/(getcolor)/page.tsx
@@ -1,5 +1,16 @@
+import Link from "next/link";
 import "../../index.css";
+import { Aperture } from "lucide-react";
 
 export default function HomePage() {
-  return <div>Home Page - where the magic happens</div>;
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-background">
+      <Link
+        href="/app"
+        className="flex items-center gap-3 rounded-md bg-primary text-primary-foreground hover:bg-primary/90 px-3 py-3 transition-all"
+      >
+        <Aperture className="h-4 w-4" /> Launch Color Picker
+      </Link>
+    </div>
+  );
 }
